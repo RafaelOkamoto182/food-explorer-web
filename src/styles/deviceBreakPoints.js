c307db5c452fcbf15fa2976b76dba619c403fdeb
Rafael Okamoto <rafa.mg1@hotmail.com>
2023-12-13 13:45:12 -0300

    itializing project from other projects pattern

---
+++ src/styles/deviceBreakPoints.js
@@ -0,0 +1,11 @@
+export const DEVICE_BREAKPOINTS = {
+    XS: "320px",
+    SM: "425px",
+    MD: "768px",
+    LG: "1024px",
+    XL: "1280px"
+}
+
+//ao trabalhar com responsividade, o que importa mesmo é a largura.
+//Claro que faz-se os ajustes para altura, mas o limitante e que deve
+//ser levado mais em consideração é a largura.
