8d10f20a7dd4fbdfe57d95ca5c9d8165bb5e1b73
Rafael Okamoto <rafa.mg1@hotmail.com>
2024-01-09 16:49:34 -0300

    installed swiper

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -11,9 +11,7 @@ export function Home() {
       <Section title={"Main dishes"}>
         <FoodCard foodData={{ name: "Fattoush", price: 25, isfavorite: false, description: "Fatuche, ou Fattoush, (em árabe فتوش, «pão úmido», pronunciado /fatˈtuːʃ/) é uma salada típica da culinária libanesa dos países da região do Levante, entre eles a Síria e o Líbano. A salada é elaborada com diferentes verduras e legumes, como tomate e rabanete, servidos junto com pão libanes (ou pita) torrado, cortado em cubos ou tiras[1]. Em comparação com o tabule, as verduras" }} />
       </Section>
-      <Section title={"Main dishes"}>
-        <FoodCard foodData={{ name: "Fattoush", price: 25, isfavorite: false, description: "Fatuche, ou Fattoush, (em árabe فتوش, «pão úmido», pronunciado /fatˈtuːʃ/) é uma salada típica da culinária libanesa dos países da região do Levante, entre eles a Síria e o Líbano. A salada é elaborada com diferentes verduras e legumes, como tomate e rabanete, servidos junto com pão libanes (ou pita) torrado, cortado em cubos ou tiras[1]. Em comparação com o tabule, as verduras" }} />
-      </Section>
+
     </Container>
   );
 }
